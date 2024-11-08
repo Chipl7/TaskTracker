ea784d0d166058c32b340218061c05452e27843a
Artem <artem.chipl@gmail.com>
2024-11-08 16:04:52 +0700

    Add third music class, scope prototype and musicPlayer.properties

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,8 +9,22 @@ public class TestSpring {
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
+
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer);
+        System.out.println(musicPlayer1);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        musicPlayer1.setVolume(-100);
+        musicPlayer1.setName("name Some");
+
+
+        System.out.println(musicPlayer1.getName());
+        System.out.println(musicPlayer1.getVolume());
 
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
