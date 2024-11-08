eba9aea1e044836d69e8d27c40d43ab390209919
Artem <artem.chipl@gmail.com>
2024-11-08 17:34:13 +0700

    Add List<Music> and new method

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -26,17 +26,28 @@ public class MusicPlayer {
     }
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public List<Music> getMusicList() {
+        return musicList;
     }
 
+    public void setMusicList(List<Music> music) {
+        this.musicList = music;
+    }
+
+
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+//
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for(Music music : musicList) {
+            System.out.println(music.getSong());
+        }
     }
 }
