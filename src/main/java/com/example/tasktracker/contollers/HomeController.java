66de2f04a3409bfdf1c4762604d673f30f78e4b9
Artem <artem.chipl@gmail.com>
2025-01-09 15:18:41 +0700

    add homeController and home.html

---
+++ src/main/java/com/example/tasktracker/contollers/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.tasktracker.contollers;
+
+import com.example.tasktracker.model.Task;
+import com.example.tasktracker.model.TasksList;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+//    @ModelAttribute(name = "task")
+//    public Task task() {
+//        return new Task();
+//    }
+
+    @ModelAttribute(name = "tasksList")
+    public TasksList tasksList() {
+        return new TasksList();
+    }
+
+    @PostMapping
+    public String processTask(@ModelAttribute TasksList tasksList) {
+        return "redirect:/task/add";
+    }
+}
