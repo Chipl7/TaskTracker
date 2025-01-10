9901deabff19ddf95f7a5ed40aa62f58bded36ac
Artem <artem.chipl@gmail.com>
2025-01-10 17:02:11 +0700

    add getters and setters(need fix lombok)

--- src/main/java/com/example/tasktracker/model/TasksList.java
+++ src/main/java/com/example/tasktracker/model/TasksList.java
@@ -1,17 +1,24 @@
 package com.example.tasktracker.model;
 
-
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Data //TODO
 public class TasksList {
 
     private List<Task> tasks = new ArrayList<>();
 
     public void addTask(Task task) {
         this.tasks.add(task);
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
