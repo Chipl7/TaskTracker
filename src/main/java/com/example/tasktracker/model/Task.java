f69b84dfa695cbe2c0055be2b5cca348aace58f7
Artem <artem.chipl@gmail.com>
2025-01-10 17:02:05 +0700

    add getters and setters(need fix lombok)

--- src/main/java/com/example/tasktracker/model/Task.java
+++ src/main/java/com/example/tasktracker/model/Task.java
@@ -2,17 +2,51 @@ package com.example.tasktracker.model;
 
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
-@Data
+@Data //TODO
 public class Task {
 
     private Long id;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date = new Date();
 
     private String name;
 
     private String descriptions;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
 }
