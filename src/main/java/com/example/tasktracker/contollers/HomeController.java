095d40de57f898bb3fd326422ee70aaf64222804
Artem <artem.chipl@gmail.com>
2025-01-10 17:01:00 +0700

    add SessionAttributes

--- src/main/java/com/example/tasktracker/contollers/HomeController.java
+++ src/main/java/com/example/tasktracker/contollers/HomeController.java
@@ -1,26 +1,20 @@
 package com.example.tasktracker.contollers;
 
-import com.example.tasktracker.model.Task;
 import com.example.tasktracker.model.TasksList;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("tasksList")
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
-
-//    @ModelAttribute(name = "task")
-//    public Task task() {
-//        return new Task();
-//    }
 
     @ModelAttribute(name = "tasksList")
     public TasksList tasksList() {
