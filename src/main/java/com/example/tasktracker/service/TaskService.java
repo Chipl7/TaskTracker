be8a714a60e0b895de157d3634b2195f235980d4
Artem <artem.chipl@gmail.com>
2025-01-16 15:20:46 +0700

    add TaskService

--- src/main/java/com/example/tasktracker/service/TaskService.java
+++ src/main/java/com/example/tasktracker/service/TaskService.java
@@ -1,11 +1,15 @@
 package com.example.tasktracker.service;
 
+import com.example.tasktracker.excption.TaskNotFoundException;
 import com.example.tasktracker.model.Task;
 import com.example.tasktracker.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,4 +27,22 @@ public class TaskService {
     public List<Task> findAll() {
         return taskRepository.findAll();
     }
+
+    public Task findById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
+    }
+
+    public Optional<Task> update(Task newTask, Long id) {
+        return taskRepository.
+                findById(id).map(task -> {
+                    task.setName(newTask.getName());
+                    task.setDescriptions(newTask.getDescriptions());
+                    return taskRepository.save(task);
+                });
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
